8f8695af216cefaf4c3c137f0f6b42f0a437bff9
Kevin Marlow <kevinmmarlow@gmail.com>
2021-03-27 14:47:53 +0800

    Update api key

--- src/api/etherscan.ts
+++ src/api/etherscan.ts
@@ -1,5 +1,8 @@
 import Axios from 'axios'
 
+const apiKey = import.meta.env.VITE_ETHERSCAN_API_KEY
+const wrappedMoonCatsTokenAddress = '0x7c40c393dc0f283f318791d746d894ddd3693572'
+
 export const getWrappedId = (originalId: string) => {
   const url = 'https://api.etherscan.io/api'
 
@@ -8,9 +11,9 @@ export const getWrappedId = (originalId: string) => {
     action: 'getLogs',
     fromBlock: '1',
     toBlock: 'latest',
-    address: '0x7c40c393dc0f283f318791d746d894ddd3693572',
+    address: wrappedMoonCatsTokenAddress,
     topic1: originalId + '000000000000000000000000000000000000000000000000000000',
-    apikey: '8IGPJFDKW255KIQZKYNQ3Z4WFEPJ3PXTRH',
+    apikey: apiKey,
   }
 
   return Axios.get(url, { params })
@@ -23,11 +26,11 @@ export const getOriginalId = (hex: string) => {
     action: 'getLogs',
     fromBlock: '1',
     toBlock: 'latest',
-    address: '0x7c40c393dc0f283f318791d746d894ddd3693572',
+    address: wrappedMoonCatsTokenAddress,
     topic0: '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
     topic1: '0x0000000000000000000000000000000000000000000000000000000000000000',
     topic3: hex,
-    apiKey: '8IGPJFDKW255KIQZKYNQ3Z4WFEPJ3PXTRH',
+    apiKey: apiKey,
   }
 
   return Axios.get(url, { params })
@@ -40,16 +43,15 @@ export const getBlock = (block: string) => {
     action: 'getLogs',
     fromBlock: block,
     toBlock: block,
-    address: '0x7c40c393dc0f283f318791d746d894ddd3693572',
+    address: wrappedMoonCatsTokenAddress,
     topic0: '0x82db4ed538dc0007cca5910a46d167b9ada30509d730f441f8fd8e426b1dbecb',
-    apikey: 'JAE544SPUKRNM5Q6Y67WX41TX3EMGPRC3P',
+    apikey: apiKey,
   }
 
   return Axios.get(url, { params })
 }
 
 export const getOpenSeaAsset = (wrappedId: string) => {
-  const url =
-    'https://api.opensea.io/api/v1/asset/0x7c40c393dc0f283f318791d746d894ddd3693572/' + wrappedId
+  const url = `https://api.opensea.io/api/v1/asset/${wrappedMoonCatsTokenAddress}/${wrappedId}`
   return Axios.get(url)
 }
